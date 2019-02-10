a3ecc991f840cb739c8645ce9c505a81f391dd7a
Jazmine Uy <juy@ucsb.edu>
2019-02-09 18:05:54 -0800

    oak point tab added

---
+++ Points/app.R
@@ -0,0 +1,58 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinythemes)
+
+# Define UI for application that displays data for fog scenarios on SRI and SCR
+ui <- navbarPage("Oak Nuts ;)", theme = shinytheme("flatly"),
+                 
+                 # Application title
+                 tabPanel("Oak Points",
+                          
+                          mainPanel(
+                            tabsetPanel(
+                              tabPanel("Santa Cruz"),
+                              tabPanel("Santa Rosa",
+                                       sidebarPanel(
+                                         radioButtons("age", "Choose Age Group:",
+                                                      c("Seedlings" = "seedsap",
+                                                        "Adults" = "adult"))
+                                       )
+                                       
+                              )
+                            )
+                          )
+                          
+                          
+                          
+                 ),
+                 
+                 
+                 tabPanel("Fog Scenarios"),
+                 tabPanel("Climate Scenarios")
+                 
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
