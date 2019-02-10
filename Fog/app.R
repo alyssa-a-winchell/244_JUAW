a946c2baf6fb122fb8ad0693415d5b68b2e30ef2
Alyssa <alyssa_a_winchell@ucsb.edu>
2019-02-09 19:31:14 -0800

    added a proto-slider

--- Fog/app.R
+++ Fog/app.R
@@ -21,7 +21,13 @@ ui <- navbarPage("Hot details from this slutty oak's secret Life!", theme = shin
               sidebarPanel(
                 selectInput("fog_scen", label = h3("Fog Scenarios"), 
                             choices = list("Constant" = 1, "Increase" = 2, "Decrease" = 3, "Elevation Threshold" = 4), 
-                            selected = 1)
+                            selected = 1),
+                # Input: Custom currency format for with basic animation ----
+                sliderInput("time", "Time Periods",
+                            min = 1981, max = 2099,
+                            value = 0, step = 30,
+                            pre = "$", sep = ",",
+                            animate = TRUE)
                 
               ),
               
@@ -35,17 +41,31 @@ ui <- navbarPage("Hot details from this slutty oak's secret Life!", theme = shin
               )
             )),
    tabPanel("Climate")
-   
-   #adds tabs on the top for all of our sections
+   #above adds tabs on the top for all of our sections
    
    
 
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to call the correct raster
 server <- function(input, output) {
+  # Reactive expression to create data frame of all input values ----
+  sliderValues <- reactive({
+    
+    data.frame(
+      Name = c("Time Period"),
+      Value = as.character(c(input$time)),
+      stringsAsFactors = FALSE)
+    
+  })
   
-  output$value <- renderPrint({ input$fog_scen })
+
+  # Show the values in an HTML table ----
+  output$values <- renderTable({
+    sliderValues()
+  })
+  
+  
   
   
 }
