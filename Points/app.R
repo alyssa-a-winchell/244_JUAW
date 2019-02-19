589089da6a53ce235ff154f32278f4ad8b3d6d76
Jazmine Uy <juy@ESM.UCSB.EDU>
2019-02-19 13:41:37 -0800

    added color changing widget

--- Points/app.R
+++ Points/app.R
@@ -8,16 +8,18 @@
 #
 
 library(shiny)
-
 library(shinythemes)
 library(sf)
 library(raster)
 library(leaflet)
+library(RColorBrewer)
 
+
+# Read in the data
 combo <- read.csv("G:/data/GitHub/244_JUAW/app/data/oaks/sri/combo.csv")
 scr_points <- read.csv("G:/data/GitHub/244_JUAW/app/data/oaks/scr/all_4326.csv")
 
-
+# Make icon for the maps as marker image. However it would work better with circles
 leaf <- makeIcon(
   iconUrl = "G:/data/GitHub/244_JUAW/app/data/oaks/leaf_icon.png", 
   iconWidth = 50, iconHeight = 50)
@@ -31,18 +33,17 @@ ui <- navbarPage("Oak Nuts ;)", theme = shinytheme("flatly"),
                  # Application title
                  tabPanel("Oak Points",
                           
-                          mainPanel(
+                          
+                          mainPanel(selectInput("points_colors", "Choose Color:",
+                                                c("Blue" = "#004d99",
+                                                  "Red" = "darkred",
+                                                  "Green" = "yellowgreen",
+                                                  "Purple" = "#8a4f7e",
+                                                  "Orange" = "#d9a440")),
                             tabsetPanel(
                               tabPanel("Santa Cruz",
-                                       selectInput("points_colors", "Choose Color:",
-                                                   c("Blue" = "blue",
-                                                     "Red" = "red",
-                                                     "Green" = "green",
-                                                     "Purple" = "purple",
-                                                     "Yellow" = "yellow",
-                                                     "Orange" = "orange")),
-                                       
                                        leafletOutput("SCRpoints")),
+                              
                               tabPanel("Santa Rosa",
                                        sidebarPanel(
                                          radioButtons("age", "Choose Age Group:",
@@ -70,11 +71,16 @@ ui <- navbarPage("Oak Nuts ;)", theme = shinytheme("flatly"),
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  points_color <- reactive({
+    input$points_colors
+    
+  })
+  
   output$SCRpoints <- renderLeaflet({
     leaflet() %>% 
-      addTiles() %>% 
+      addProviderTiles(providers$Esri.WorldImagery) %>% 
       setView(lng = -119.722862, lat = 34.020433, zoom = 11) %>% 
-      addMarkers(data = scr_points, lng = ~POINT_X, lat = ~POINT_Y, icon = leaf)
+      addCircleMarkers(radius = 4, fillColor = points_color(), stroke = FALSE, fillOpacity = 0.5, data = scr_points, lng = ~POINT_X, lat = ~POINT_Y)
     
   })
   
@@ -82,9 +88,14 @@ server <- function(input, output) {
     combo[ combo$Age == input$age, ]
   })
   
+  sri_color <- reactive({
+    input$points_colors
+    
+  })
+  
   output$SRIpoints <- renderLeaflet({
     leaflet() %>% 
-      addTiles() %>% 
+      addProviderTiles(providers$Esri.WorldImagery) %>% 
       setView(lng = -120.107103, lat = 33.968757, zoom = 11) 
     
   })
@@ -94,7 +105,7 @@ server <- function(input, output) {
     
     leafletProxy("SRIpoints") %>%
       clearMarkers() %>% 
-      addMarkers(data = filteredData(), lng = ~POINT_X, lat = ~POINT_Y, icon = leaf)
+      addCircleMarkers(data = filteredData(), lng = ~POINT_X, lat = ~POINT_Y, radius = 4, fillColor = points_color(), stroke = FALSE, fillOpacity = 0.5)
   })
   
   
