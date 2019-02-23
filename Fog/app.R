8b426200ec3c533209af6e7aa9f6703b9070195a
Alyssa Winchell <awinchell@ESM.UCSB.EDU>
2019-02-22 17:39:19 -0800

    it is fixed!

--- Fog/app.R
+++ Fog/app.R
@@ -23,9 +23,8 @@ ui <- navbarPage("Hot details from this slutty oak's secret Life!", theme = shin
             # Select box with options for fog scenarios
             sidebarLayout(
               sidebarPanel(
-                selectInput("fogscen", label = h3("Fog Scenarios"), 
-                            choices = list("Constant" = 1, "Increase" = 2, "Decrease" = 3, "Elevation Threshold" = 4), 
-                            selected = 1),
+                selectInput("fogscen", "Fog Scenarios", 
+                            choices = c("Constant", "Increase", "Decrease", "Elevation Threshold")),
                 # Input: Custom 30 yr periods format with basic animation
                 sliderTextInput("timeperiods","Time Periods" , 
                                 choices = c("1981-2010", "2010-2039", "2040-2069", "2070-2099"),
@@ -52,24 +51,24 @@ ui <- navbarPage("Hot details from this slutty oak's secret Life!", theme = shin
 # Define server logic required to call the correct raster
 server <- function(input, output) {
   # Reactive expression to create data frame of all input values ----
-  sliderValues <- reactive({
-    
-    data.frame(
-      Name = c("Time Period"),
-      Value = as.character(c(input$time)),
-      stringsAsFactors = FALSE)
-    
-  })
+  # sliderValues <- reactive({
+  #   
+  #   data.frame(
+  #     Name = c("Time Period"),
+  #     Value = as.character(c(input$time)),
+  #     stringsAsFactors = FALSE)
+  #   
+  # })
   
 
   output$scrmap <- renderLeaflet({
     
     
     scen<-switch(input$fogscen,
-                 "Constant Fog"=scen<-"const", 
-                 "Fog Increase"=scen<-"inc", 
-                 "Fog Decrease"=scen<-"dec", 
-                 "Fog Elevation Threshold"=scen<-"elev")
+                 "Constant"=scen<-"const", 
+                 "Increase"=scen<-"inc", 
+                 "Decrease"=scen<-"dec", 
+                 "Elevation Threshold"=scen<-"elev")
 
     time<-switch(input$timeperiods,
                  "1981-2010"=time<-"historic",
@@ -78,18 +77,18 @@ server <- function(input, output) {
                  "2070-2099"=time<-"2070_2099")
     
     
-    scr<-raster(paste0("app/data/fog/scr/",scen,"/", time, ".tif")) 
+    scr<-raster(paste0("data/fog/scr/",scen,"/", time, ".tif"))
     proj4string(scr) <- CRS("+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
     
     
     leaflet() %>% addTiles() %>%
-      addRasterImage(scr, colors = "Blues", opacity = 0.8) %>%
+      addRasterImage(scr, colors = "Blues", opacity = 0.8) 
       #setView(lng=-13388304, lat=4012916, zoom=20) %>% #Figure out how to set view extent
-      addLegend("topright", pal = "Blues", values = values(scr),
-                title = "Probability of Fog Inundation", 
-                labFormat = labelFormat(transform=function(scr) sort (scr, decreasing=FALSE))) #decreasing false until can figure out how to reverse legend colors
+      # addLegend("topright", pal = "Blues", values = values(scr),
+      #           title = "Probability of Fog Inundation", 
+      #           labFormat = labelFormat(transform=function(scr) sort (scr, decreasing=FALSE))) #decreasing false until can figure out how to reverse legend colors
     
-  }) #end render leaflet
+}) #end render leaflet
   
   output$srimap <- renderLeaflet({
     
